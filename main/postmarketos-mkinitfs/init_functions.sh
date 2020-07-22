porthole-dev/pmaports

commit 7c433ad396e4555204c83daf9e4e2980b7797fbb

main/postmarketos-mkinitfs: rm pmOS_deleteme (MR 1428)
Remove the "pmOS_deleteme" partition left behind by the on-device
installer, if it exists. Let the existing resize_root_partition() extend
the root partition over the newly gained space (and rest of the storage
device) right afterwards.

main/postmarketos-mkinitfs/APKBUILD
@@ -1,5 +1,5 @@
 pkgname=postmarketos-mkinitfs
-pkgver=0.13.2
+pkgver=0.14.0
 pkgrel=0
 pkgdesc="Tool to generate initramfs images for postmarketOS"
 url="https://postmarketos.org"
@@ -39,8 +39,8 @@ check() {
 }
 
 sha512sums="5037cb7285bb7c0c40ca9e6df332d882ef9a8b379756c785f921e062dab1b7e7f3139d00897f69323a916d709ced4297fea8cbd3a13ebae575b873ec9e2cbfae  00-default.modules
-995b4d15e6b248d55d53221ec52674e62d74f0be539ac2ab26d4279544e2bece7aa58810dd831e1b2f08f0520fc9a8dff3ad4df52a46ef73f61e462f74f68c48  init.sh.in
-c8164e0cc902b63e6dc9950c19828ec6bdb91248b48552f3504e2763be12a4d7a5b240f4e6a0100dce2eb8e9004502a05aefb354f02e4b06092a1ea6fc9abd8c  init_functions.sh
+7fc6ba96419628850984df3322b73bcf655cba03fe3e97ec7425e66e90f4b2a4de106b5cb2e3d46785a92557334288011334104285e2050c7a8e823b8fa5668c  init.sh.in
+8237cfa4c7aabd6a71fcf2541a9d3f74269b5c00f3c0519c97b801da8b280527a0a8c3e1eff2a67b46859ee604a3e4519329cbfb58defe9739df249eb8646c1e  init_functions.sh
 7201d4640a3e9ead8a47ffd9916b068476b7d950760a84c37e02268c942d49896bc986da6bdee27e832639c90775354d68046a6475205d8d1da995b068120d8f  mkinitfs.sh
 d593b921bd6c0204af874266060667d78a232c0131ce7f0ad4124f9b240fcbb6ac2a8a2b8ee30d27d78bed8c1a506c4e6cb13b57c33cee3a00a7d96eca45a7f3  mkinitfs_functions.sh
 c7a3c33daeb12b33ac72207191941c4d634f15c22958273b52af381a70ebaba1d3a9299483f0c447d9e66c560151fe7b9588bb4bbef2c8914f83185984ee4622  mkinitfs_test.sh"

main/postmarketos-mkinitfs/init_functions.sh
@@ -211,6 +211,21 @@ wait_root_partition() {
 	done
 }
 
+delete_old_install_partition() {
+	# The on-device installer leaves a "pmOS_deleteme" (p3) partition after
+	# successful installation, located after "pmOS_root" (p2). Delete it,
+	# so we can use the space.
+	partition="$(find_root_partition | sed 's/2$/3/')"
+	if ! blkid "$partition" | grep -q pmOS_deleteme; then
+		return
+	fi
+
+	device="$(echo "$partition" | sed -E 's/3$//')"
+	echo "First boot after running on-device installer - deleting old" \
+		"install partition: $partition"
+	parted -s "$device" rm 3
+}
+
 # $1: path to device
 has_unallocated_space() {
 	# Check if there is unallocated space at the end of the device

main/postmarketos-mkinitfs/init.sh.in
@@ -39,6 +39,7 @@ mount_boot_partition /boot
 extract_initramfs_extra @INITRAMFS_EXTRA@
 start_charging_mode
 wait_root_partition
+delete_old_install_partition
 resize_root_partition
 unlock_root_partition
 resize_root_filesystem

commit 334cc2d8a8aeb04bb2099c293c4b4e7399a3d21d

main/postmarketos-mkinitfs: fix device sed regex (MR 1436)
The p? is not redundant. Without it, the p in mmcblk0p2 for example does
not get cut off, meaning the resulting device is not "mmcblk0" but
"mmcblk0p". My bad, sorry for the breakage.

Fixes: 7c433ad3 ("main/postmarketos-mkinitfs: rm pmOS_deleteme")
Fixes: a329bd59 ("main/postmarketos-mkinitfs: resize: unallocated space check, even if forced")

main/postmarketos-mkinitfs/APKBUILD
@@ -1,6 +1,6 @@
 pkgname=postmarketos-mkinitfs
 pkgver=0.14.0
-pkgrel=0
+pkgrel=1
 pkgdesc="Tool to generate initramfs images for postmarketOS"
 url="https://postmarketos.org"
 # multipath-tools: kpartx
@@ -40,7 +40,7 @@ check() {
 
 sha512sums="5037cb7285bb7c0c40ca9e6df332d882ef9a8b379756c785f921e062dab1b7e7f3139d00897f69323a916d709ced4297fea8cbd3a13ebae575b873ec9e2cbfae  00-default.modules
 7fc6ba96419628850984df3322b73bcf655cba03fe3e97ec7425e66e90f4b2a4de106b5cb2e3d46785a92557334288011334104285e2050c7a8e823b8fa5668c  init.sh.in
-8237cfa4c7aabd6a71fcf2541a9d3f74269b5c00f3c0519c97b801da8b280527a0a8c3e1eff2a67b46859ee604a3e4519329cbfb58defe9739df249eb8646c1e  init_functions.sh
+e9bbf6850c0c44f10c8fe66ceb05ed454844646dd937b5fe614810a0cce7c7383febe5ee1b0830217bff71a29621d82aa57adbcd59cb24a72701504887513ff1  init_functions.sh
 7201d4640a3e9ead8a47ffd9916b068476b7d950760a84c37e02268c942d49896bc986da6bdee27e832639c90775354d68046a6475205d8d1da995b068120d8f  mkinitfs.sh
 d593b921bd6c0204af874266060667d78a232c0131ce7f0ad4124f9b240fcbb6ac2a8a2b8ee30d27d78bed8c1a506c4e6cb13b57c33cee3a00a7d96eca45a7f3  mkinitfs_functions.sh
 c7a3c33daeb12b33ac72207191941c4d634f15c22958273b52af381a70ebaba1d3a9299483f0c447d9e66c560151fe7b9588bb4bbef2c8914f83185984ee4622  mkinitfs_test.sh"

main/postmarketos-mkinitfs/init_functions.sh
@@ -220,7 +220,7 @@ delete_old_install_partition() {
 		return
 	fi
 
-	device="$(echo "$partition" | sed -E 's/3$//')"
+	device="$(echo "$partition" | sed -E 's/p?3$//')"
 	echo "First boot after running on-device installer - deleting old" \
 		"install partition: $partition"
 	parted -s "$device" rm 3
@@ -264,7 +264,7 @@ resize_root_partition() {
 	# PinePhone). For them, it is fine to use the whole storage device and
 	# so we pass PMOS_FORCE_PARTITION_RESIZE as kernel parameter.
 	if grep -q PMOS_FORCE_PARTITION_RESIZE /proc/cmdline; then
-		partition_dev="$(echo "$partition" | sed -E 's/2$//')"
+		partition_dev="$(echo "$partition" | sed -E 's/p?2$//')"
 		if has_unallocated_space "$partition_dev"; then
 			echo "Resize root partition ($partition)"
 			parted -s "$partition_dev" resizepart 2 100%
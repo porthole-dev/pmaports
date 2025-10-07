0eb2b181adc58bdf19e67f64c0657114ba0f1865
Robert Eckelmann <longnoserob@postmarketos.org>
2025-10-07 20:59:23 +0900

    CI: correct description of docs build tools
    Signed-off-by: Robert Eckelmann <longnoserob@postmarketos.org>
    Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/7151>

--- .ci/build-docs.sh
+++ .ci/build-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # Copyright 2025 Pablo Correa Gomez
 # SPDX-License-Identifier: GPL-3.0-or-later
-# Description: create documentation with sphinx
+# Description: create documentation with antora
 # Options: with-dot-git
 # Artifacts: build/site
 # https://postmarketos.org/pmb-ci

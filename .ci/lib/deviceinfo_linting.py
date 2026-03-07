70b7a6346e3c3cadfc7b229d7a43eb0cc18109e1
Luca Weiss <luca@lucaweiss.eu>
2026-03-07 15:37:58 +0100

    CI: Run dint job on all files on deviceinfo_schema.toml change
    Similar to kconfigcheck.toml, let's run the dint job on all deviceinfo
    files when the spec itself is changed. This allows to catch possible new
    warnings and errors early and fix them.
    
    Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/8139>

--- .ci/lib/deviceinfo_linting.py
+++ .ci/lib/deviceinfo_linting.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import common
+import glob
 import os.path
 import subprocess
 import sys
@@ -11,9 +12,15 @@ if __name__ == "__main__":
     if common.commit_message_has_string("[ci:skip-dint]"):
         print("WARNING: not linting deviceinfo files ([ci:skip-dint])")
         exit(0)
-    # only lint deviceinfo files in the devices repo
-    deviceinfo_files = {file for file in common.get_changed_files()
-                        if os.path.basename(file) == "deviceinfo" and file.startswith("device/")}
+
+    if "deviceinfo_schema.toml" in common.get_changed_files():
+        print("deviceinfo_schema.toml changed -> checking all deviceinfo files")
+        pmaports_dir = common.get_pmaports_dir()
+        deviceinfo_files = set(glob.glob(pmaports_dir + "/device/*/*/deviceinfo"))
+    else:
+        # only lint deviceinfo files in the devices repo
+        deviceinfo_files = {file for file in common.get_changed_files()
+                            if os.path.basename(file) == "deviceinfo" and file.startswith("device/")}
 
     try:
         subprocess.run(["dint", "check", *deviceinfo_files], text=True, check=True)

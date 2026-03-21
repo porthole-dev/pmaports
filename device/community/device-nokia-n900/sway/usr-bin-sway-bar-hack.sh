1cb2c7a9b41be54675f4b73a81181c258d5fbf3b
Sicelo A. Mhlongo <absicsz@gmail.com>
2026-03-21 23:05:41 +0200

    device-nokia-n900: simplify sway status bar configuration
    Since version 4, postmarketos-ui-sway allows overriding the status bar easily,
    thus the hack to hide the global sway status bar to display a second bar is no
    longer necessary.
    
    Signed-off-By: Sicelo A. Mhlongo <absicsz@gmail.com>
    Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/8472>

--- device/community/device-nokia-n900/sway/usr-bin-sway-bar-hack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Stop the default bar. For unknown reasons, these commands do not work from
-# inside the config
-(
-	sleep 3
-	swaymsg bar bar-0 status_command -
-	swaymsg bar mode invisible bar-0
-)  &
